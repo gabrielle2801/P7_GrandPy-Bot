70730c954e0eb4ce4c00e5c1d81a8c06778711c3
Gabrielle Azadian <gabrielleazadian@gmail.com>
2021-05-05 15:15:22 +0200

    Updating css and index put loading on button

--- GrandPy_Bot/static/js/script.js
+++ GrandPy_Bot/static/js/script.js
@@ -20,35 +20,48 @@ function postFormData(url, data){
 
 form.addEventListener("submit", function(event){
     event.preventDefault();
-    $('#btn-one').html('<span class="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span>Loading...').attr('disabled', true);
-    setTimeout(function(){
-        document.getElementById('userText')
-    }, 1000)
+    spinner()
     postFormData("/post_address", new FormData(form))
     .then(response => {
         console.log(response);
+        spinner()
+
         if (response.error){
             document.getElementById('address_geocode').textContent = response.error;
         }else{
-        let address =response['sentences']+response['formatted_address'];
-        document.getElementById('address_geocode').textContent = response['formatted_address'];
-        document.getElementById('address_geocode').textContent=address;
-        document.getElementById('extract_wiki').textContent = response['extract'];
-        initMap(response['lat'],response['lng']);
-        map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 15,
-      center: new google.maps.LatLng(response['lat'], response['lng'])
-    });
-        var marker = new google.maps.Marker({position: {lat: response['lat'], lng: response['lng']},
-    map: map});
-        var newDiv = document.createElement("div")
+
+            setTimeout(()=> {extract(response);},2500);
+
+
+        /*var newDiv = document.createElement("div");
         newDiv.textContent='Hi there and greetings!';
         var currentDiv = document.getElementById('extract_wiki');
         currentDiv.appendChild(newDiv);
-        document.getElementById("userText").value="";
+        document.getElementById("userText").value="";*/
+
     }
-
     })
-
 })
 
+function spinner(){
+     $('#btn-one').html('<span class="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span>Loading...').attr('disabled', true);
+}
+
+function extract(response){
+    console.log('essai loading...')
+    let address =response['sentences']+response['formatted_address'];
+    document.getElementById('address_geocode').textContent = response['formatted_address'];
+    document.getElementById('address_geocode').textContent=address;
+    document.getElementById('extract_wiki').textContent = response['extract'];
+    initMap(response['lat'],response['lng']);
+    document.getElementById('map').style.height = "400px";
+    map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: new google.maps.LatLng(response['lat'], response['lng'])
+    });
+    var marker = new google.maps.Marker({position: {lat: response['lat'], lng: response['lng']},
+    map: map});
+    $('#btn-one').html('Envoyer ...').attr('disabled', false);
+    document.getElementById("user-text-form").reset();
+
+}
