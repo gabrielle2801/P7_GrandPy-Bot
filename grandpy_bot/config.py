2b8ab6502814921afab7d91c3ad25defe44b1bb7
Gabrielle Azadian <gabrielleazadian@gmail.com>
2021-05-21 20:46:17 +0200

    delete directory

--- grandpy_bot/config.py
+++
@@ -1,4 +0,0 @@
-# To generate a new secret key:
-# >>> import random, string
-# >>> "".join([random.choice(string.printable) for _ in range(24)])
-SECRET_KEY = "e6tl-+&*132nf^rv&_-jve0)nr40d1n+lub(&91$qo@+rd)gti"
