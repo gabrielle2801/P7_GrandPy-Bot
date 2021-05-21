0d02a1be8298e2def7745e58b92e09b194050179
Gabrielle Azadian <gabrielleazadian@gmail.com>
2021-05-21 20:58:41 +0200

    Restore files

---
+++ grandpy_bot/parser.py
@@ -0,0 +1,17 @@
+import re
+from grandpy_bot.constant import PATTERN_PARSER
+
+
+class MatchNotFound(Exception):
+    pass
+
+
+def parse_search(user_request):
+    for p in PATTERN_PARSER:
+        response = re.search(p, user_request)
+        if response:
+            result_regex = response.group(1).strip()
+            return result_regex
+    if not response:
+        raise MatchNotFound(
+            "Oospy demande incorrect, reformulez votre demande")
